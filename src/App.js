9bdd14dc384716435104a0c41b6a262b19bceab4
kyleschneiderx <kyleschneiderx@gmail.com>
2021-06-10 18:55:02 -0600

    added in key

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     const US = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
     const render_list= []
     for(let i=0; i<US.length; i++){
-      render_list.push(<option value={US[i].toLowerCase()}>{US[i]}</option>)
+      render_list.push(<option key={US[i]} value={US[i].toLowerCase()}>{US[i]}</option>)
     }
     return(
       render_list
